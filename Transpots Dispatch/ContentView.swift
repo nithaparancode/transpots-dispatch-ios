16ab69376d2a5b3e7d01bc81f12cd4ef06e65c93
nithapran <nithaparanf@gmail.com>
2026-01-20 23:07:02 -0500

    initial commit

---
+++ Transpots Dispatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transpots Dispatch
+//
+//  Created by Nithaparan Francis on 2026-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transpots Dispatch/Transpots_DispatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transpots_DispatchApp.swift
+//  Transpots Dispatch
+//
+//  Created by Nithaparan Francis on 2026-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Transpots_DispatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
